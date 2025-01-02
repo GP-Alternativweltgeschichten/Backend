c460d0f5369d2c0b1471dc6e98e8660bb61049bb
Hanna Sophie Müller <mhannasophie@gmail.com>
2025-01-02 10:23:23 +0100

    map endpoints

--- src/main/java/com/be/backend/maps/MapController.java
+++ src/main/java/com/be/backend/maps/MapController.java
@@ -14,5 +14,30 @@ import java.util.List;
 public class MapController {
     private final MapService MapService;
 
+    @GetMapping("")
+    public ResponseEntity<List<MapDTO>> getAllMaps() {
+        return ResponseEntity.ok(MapService.getAllMaps().stream().map(MapMapper::toDTO).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MapDTO> getMapById(@PathVariable Integer id) {
+        return ResponseEntity.ok(MapMapper.toDTO(MapService.getMapById(id)));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<MapDTO> saveMap(@RequestBody Map Map) {
+        return ResponseEntity.ok(MapMapper.toDTO(MapService.saveMap(Map)));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<MapDTO> updateMap(@RequestBody Map Map) {
+        return ResponseEntity.ok(MapMapper.toDTO(MapService.updateMap(Map)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMapById(@PathVariable Integer id) {
+        MapService.deleteMapById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/be/backend/maps/MapService.java
+++ src/main/java/com/be/backend/maps/MapService.java
@@ -14,4 +14,42 @@ import java.util.Optional;
 public class MapService {
     private final MapRepo MapRepo;
 
+    public List<Map> getAllMaps() {
+        return MapRepo.findAll();
+    }
+
+    public Map getMapById(Integer id) {
+        Optional<Map> optionalMap = MapRepo.findById(id);
+        if (optionalMap.isPresent()) {
+            return optionalMap.get();
+        }
+        log.info("Map with id {} not found", id);
+        return null;
+    }
+
+    public Map saveMap(Map Map) {
+        Map.setCreatedAt(LocalDateTime.now());
+        Map.setUpdatedAt(LocalDateTime.now());
+        Map savedMap = MapRepo.save(Map);
+
+        log.info("Map with id {} saved", savedMap.getId());
+        return savedMap;
+    }
+
+    public Map updateMap(Map Map) {
+        Optional<Map> existingMap = MapRepo.findById(Map.getId());
+        Map.setCreatedAt(existingMap.get().getCreatedAt());
+        Map.setUpdatedAt(LocalDateTime.now());
+
+        Map updatedMap = MapRepo.save(Map);
+
+        log.info("Map with id {} updated", updatedMap.getId());
+        return updatedMap;
+    }
+
+    public void deleteMapById(Integer id) {
+        MapRepo.deleteById(id);
+        log.info("Map with id {} deleted", id);
+    }
+
 }
