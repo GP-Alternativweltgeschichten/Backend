6d646a9954978df405560a864e0ee614c984dd9b
Hanna Sophie Müller <mhannasophie@gmail.com>
2025-01-09 15:51:36 +0100

    add second port for other fe

--- src/main/java/com/be/backend/BackendApplication.java
+++ src/main/java/com/be/backend/BackendApplication.java
@@ -14,7 +14,7 @@ public class BackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:4201");
 			}
 		};
 	}
