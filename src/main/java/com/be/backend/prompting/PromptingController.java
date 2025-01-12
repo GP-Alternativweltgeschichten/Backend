a46b683243321099213c2292c0f30cab769fb0e0
collin4210 <collinsasuke@gmail.com>
2025-01-12 12:37:19 +0100

    TextToImage Schnittstelle hinzugefügt.

--- src/main/java/com/be/backend/prompting/PromptingController.java
+++ src/main/java/com/be/backend/prompting/PromptingController.java
@@ -1,9 +1,13 @@
 package com.be.backend.prompting;
 
 import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/prompting")
@@ -13,7 +17,14 @@ public class PromptingController {
     private final PromptingService promptingService;
 
     @PostMapping("/text")
-    public ResponseEntity<String> getText(@RequestBody String text) {
-        return ResponseEntity.ok(promptingService.getText(text));
+    public ResponseEntity<byte[]> getImageFromText(@RequestBody String text) throws IOException, JSONException, URISyntaxException {
+
+        return ResponseEntity.ok(promptingService.getImageFromText(text));
     }
+
+
+
+
+
+
 }
--- src/main/java/com/be/backend/prompting/PromptingService.java
+++ src/main/java/com/be/backend/prompting/PromptingService.java
@@ -2,16 +2,48 @@ package com.be.backend.prompting;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PromptingService {
+    final String uri = "http://127.0.0.1:8000";
+    RestTemplate restTemplate = new RestTemplate();
 
-    public String getText(String text) {
-        log.info("Text received: {}", text);
-        return text;
+
+    public byte[] getImageFromText(String text) throws JSONException, IOException {
+      HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+
+       JSONObject jsonObject = new JSONObject(text);
+          HttpEntity<String> request = new HttpEntity<String>(text, headers);
+
+         ResponseEntity<byte[]> response =restTemplate.postForEntity(uri + "/text/", request, byte[].class);
+            byte[] imageBytes = response.getBody();
+
+
+
+
+            return imageBytes;
+
+
+
+
+
+
     }
+
+
 
 }
