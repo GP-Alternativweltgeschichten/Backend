7c851366f665e4c483cc8517aa5571c9c8a76da8
K4tara <katrin-schweitzer@t-online.de>
2025-01-30 21:26:16 +0100

    init.sql mit allen themenwelten

--- docker/init.sql
+++ docker/init.sql
@@ -1,7 +1,9 @@
 
+DROP TABLE IF EXISTS Scenario CASCADE;
+DROP TABLE IF EXISTS Old_Map CASCADE;
 
-CREATE TABLE if not exists Scenario
-(
+
+CREATE TABLE Scenario (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     image       bytea,
@@ -12,9 +14,7 @@ CREATE TABLE if not exists Scenario
     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE if not exists Old_Map
-(
+CREATE TABLE Old_Map (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     image       bytea,
@@ -26,3 +26,12 @@ CREATE TABLE if not exists Old_Map
 );
 
 
+INSERT INTO Scenario (name, image, description, editable, visible)
+VALUES
+    ('Olpe heute',pg_read_binary_file('/res/olpe_default.png')::bytea, 'Aktuelle Karte von Olpe', TRUE, TRUE),
+    ('ISEK Plan', pg_read_binary_file('/res/olpe_isek.png')::bytea,'Umsetzung des innerstädischen Entwicklungskonzepts', TRUE, TRUE),
+    ('Überschwemmung', pg_read_binary_file('/res/olpe_uberflutet.png')::bytea,'Karte des überfluteten Gebiets von Olpe', TRUE, TRUE);
+
+INSERT INTO Old_Map (name, image, dateOfMap, editable, visible)
+VALUES
+    ('Olpe damals', pg_read_binary_file('/res/olpe_1987.jpeg')::bytea,'1987', FALSE, TRUE);
