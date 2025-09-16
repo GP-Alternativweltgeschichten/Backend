b69c9712dd904fb01b3f5481f26f5883c8d273a4
chris <christian.pankiv@gmail.com>
2025-09-16 18:29:49 +0200

    reverted prior changes regarding the Chat response

--- src/main/java/com/be/backend/aiChat/AiChatController.java
+++ src/main/java/com/be/backend/aiChat/AiChatController.java
@@ -12,18 +12,12 @@ import org.springframework.web.bind.annotation.*;
 public class AiChatController {
     private final AiChatService aiChatService;
 
-    @PostMapping(value = "/text",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AiChatResponseDTO> getTextFromTextForChat(@RequestBody AiChatDTO request) {
-        String answer = aiChatService.getTextForChat(request.getText(), request.getConversationId());
-        AiChatResponseDTO out = new AiChatResponseDTO(answer,request.getConversationId());
+    @PostMapping("/text")
+    public ResponseEntity<String> getTextFromTextForChat(@RequestBody AiChatDTO request) {
         return ResponseEntity.ok(aiChatService.getTextForChat(request.text, request.conversationId));
     }
 
-    @PostMapping(value = "/image",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/image")
     public ResponseEntity<String> getImageFromTextAndImageAndMask(@RequestBody AiChatDTO request ) {
         return ResponseEntity.ok(aiChatService.getTextFromImageForChat(request.image, request.mask, request.conversationId));
     }
